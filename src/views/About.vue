36374cddd253ff6267deaf1f7a03cb9307935879
Marko Tomljanovic <markot@windowslive.com>
2021-05-24 17:03:55 +0200

    Zvjezdice

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="about">
-    about
-  </div>
+  <div id="about"><stars-rating v-bind="config"></stars-rating></div>
 </template>
+
+<script>
+import starsRating from "@/components/rating-stars";
+export default {
+  name: "about",
+  components: {
+    starsRating: starsRating,
+  },
+  data() {
+    return {
+      config: {
+        rating: 4.1,
+        isIndicatorActive: true,
+        starStyle: {
+          fullStarColor: "#ffee00",
+          emptyStarColor: "#dddddd",
+          starWidth: 50,
+          starHeight: 50,
+        },
+      },
+    };
+  },
+  computed: {},
+  methods: {},
+  created() {},
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

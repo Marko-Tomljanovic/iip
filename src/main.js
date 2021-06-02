8422015aa66ffe14526a297f2912a7ead6e8e9b1
Marko Tomljanovic <markot@windowslive.com>
2021-06-02 12:13:15 +0200

    Firebase
    + instaliran firebase
    + uspostavljena prijava
    +uspostavljena registracija
    + inportan bostrap vue css
    +kreiran firebase observale
    + izbrisane nepotrebne stvari
    + styling

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 
 import Vue from 'vue'
-
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'; 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import swal  from 'sweetalert'
 
 
 Vue.prototype.$alert = swal
 Vue.use(BootstrapVue)
-
-
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,17 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/prijava',
+    name: 'prijava',
+    component: () => import(/* webpackChunkName: "about" */ '../views/prijava.vue')
+  },
+  {
+    path: '/registracija',
+    name: 'registracija',
+    component: () => import(/* webpackChunkName: "about" */ '../views/registracija.vue')
   }
 ]
 
